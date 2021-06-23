f1587d82c74d51bcad97bb2edd501e9244f00ce9
Cristian <camilo.santamaria322@gmail.com>
2021-06-22 19:35:25 -0500

    Se agrega funcionalidad completa de moxkspy.test

--- src/ejemplo/mockspy.test.js
+++ src/ejemplo/mockspy.test.js
@@ -9,4 +9,31 @@ test("calls math.add", () => {
   expect(addMock).toHaveBeenCalledWith(1, 2);
 });
 
+//Resta
+test("calls math.subtract", () => {
+  const subtractMock = jest.spyOn(math, "subtract");
+  // calls the original implementation
+  expect(app.doSubtract(2,1)).toEqual(-1);
+  // and the spy stores the calls to add
+  expect(subtractMock).toHaveBeenCalledWith(2, 1);
+});
+
+//Multiplicación
+test("calls math.multiply", () => {
+  const multiplyMock = jest.spyOn(math, "multiply");
+  // calls the original implementation
+  expect(app.doMultiply(1, 2)).toEqual(2);
+  // and the spy stores the calls to add
+  expect(multiplyMock).toHaveBeenCalledWith(1, 2);
+});
+
+//División
+test("calls math.divide", () => {
+  const divideMock = jest.spyOn(math, "divide");
+  // calls the original implementation
+  expect(app.doDivide(2,2)).toEqual(1);
+  // and the spy stores the calls to add
+  expect(divideMock).toHaveBeenCalledWith(2, 2);
+});
+
 
